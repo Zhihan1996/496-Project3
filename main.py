cbf81ef3bec178715355f62d3dc7ea16475531e2
Zhihan1996 <zzh760998379@163.com>
2019-11-16 00:25:56 -0600

    task b partially done

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import pymysql
 
 
@@ -15,16 +16,68 @@ class DB():
         self.connect.close()
 
 
-def login(username, password, db):
-    query = "select distinct Password from student where Name=\'" + str(username) + "\'"
+def check_login(username, password, db):
+    query = "select distinct Password " \
+            "from student " \
+            "where Name=\'" + str(username) + "\'"
     success = db.execute(query)
-    data = db.fetchall()
     if success:
+        data = db.fetchall()
         for item in data:
             if item['Password'] == password:
                 return True
     return False
 
+
+def login(db):
+    while True:
+        username = input("Please type your username: \n")
+        print("\n")
+        password = input("Please type your password: \n")
+
+        if not check_login(username=username, password=password, db=db):
+            print("Log in failed, please try again")
+        else:
+            return username
+
+def menu(db, username):
+    print("\nHi, here is the Student Menu")
+    print("You courses are listed as follow:\n")
+
+    # Find local time and construct query
+    localtime = time.localtime(time.time())
+    year = localtime.tm_year
+    month = localtime.tm_mon
+    semester = "Q1" if month < 7 else "Q2"
+    query = "select distinct UoSCode " \
+            "from student, transcript " \
+            "where student.Id=transcript.StudId " \
+            "and Year=" + str(year) +" " \
+            "and Semester=" + "\'" + str(semester) + "\'" + " " \
+            "and student.Name=" + "\'" + str(username) + "\'"
+
+    # Query the result and print it
+    success = db.execute(query)
+    if success:
+        data = db.fetchall()
+        for item in data:
+            print("="*len(item["UoSCode"]))
+            print(item["UoSCode"])
+        print("=" * len(item["UoSCode"]))
+
+    # List the choices
+    print("\nYou have several options:")
+    print("Type 1 for Transcript")
+    print("Type 2 for Enroll")
+    print("Type 3 for Withdraw")
+    print("Type 4 for Personal Details")
+    print("Type 5 for Logout\n")
+    choice = input("Your choice: ")
+
+    return choice
+
+
+    
 
 def main():
     # Initiate the database
@@ -32,17 +85,9 @@ def main():
         # Initiate the interface
         print("Hi, here is the login interface\n")
 
-        while True:
-            username = input("Please type your username: \n")
-            password = input("Please type your password: \n")
-
-            if not login(username, password, db):
-                print("Log in failed, please try again")
-            else:
-                print("\nHi, here is the Student Menu\n")
-                break
-
-
+        # Login
+        username = login(db=db)
+        choice = menu(username=username,db=db)
 
 
 
